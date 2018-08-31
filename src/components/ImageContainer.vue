7800a7e92689ffcf217ee0018082875617ed5ccf
Ilya Zub <zaoooza92@gmail.com>
2018-08-31 15:14:55 +0300

    Main page adjustment

---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="image-container square">
+    <div class="image square" :style="{ backgroundImage: 'url(' + src + ')' }"></div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    props: [
+      'src'
+    ],
+  }
+</script>
+
+<style scoped>
+  .image-container {
+    background-color: #efefef;
+  }
+
+  .square {
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%;
+  }
+
+  .image {
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,20 @@
 <template>
-  <div class="container-fluid">
-    <h3 class="text-center mb-3">
-      Наши мероприятия
-    </h3>
-    <div class="row">
-      <div class="col-12 col-sm-6 col-md-4 mb-4" v-for="event in events" :key="event.id">
-        <div class="square event-content w-100 h-100" :style="{ backgroundImage: 'url(' + bgImg(event) + ')' }">
-          <div class="info">
-            <div class="title">
-              {{ event.title }}
-            </div>
-            <div class="date">
-              {{ event.date }}
-            </div>
-          </div>
+  <div class="events-list">
+    <div class="container-fluid">
+      <h3 class="text-center mb-3">
+        Наши мероприятия
+      </h3>
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-4" v-for="event in events" :key="event.id">
+          <router-link :to="{ name: 'Event', params: event }" class="event square">
+            <ImageContainer :src="bgImg(event)" />
 
-          <div class="show-more">
-            <router-link :to="{ name: 'Event', params: event }" class="link">
-              <span class="h-100 w-100 d-flex align-items-center justify-content-center">Подробнее</span>
-            </router-link>
-          </div>
+            <div class="info">
+              <div class="title">{{ event.title }}</div>
+              <div class="date">{{ event.date }}</div>
+            </div>
+            <div class="show-more">Подробнее</div>
+          </router-link>
         </div>
       </div>
     </div>
@@ -27,7 +22,12 @@
 </template>
 
 <script lang="ts">
+  import ImageContainer from "@/components/ImageContainer.vue";
+
   export default {
+    components: {
+      ImageContainer,
+    },
     props: ['events'],
     methods: {
       bgImg(event) {
@@ -40,12 +40,6 @@
 </script>
 
 <style scoped>
-  .event-content {
-    cursor: pointer;
-    position: relative;
-    background-size: cover;
-  }
-
   .info {
     position: absolute;
     bottom: 0;
@@ -57,32 +51,42 @@
     text-shadow: 1px 1px 1px #000;
   }
 
+  .event {
+    margin-bottom: 1.5rem;
+    display: block;
+    position: relative;
+  }
+
   .title {
-    font-size: 20px;
+    font-size: 1.25rem;
   }
 
   .date {
-    font-size: 14px;
+    font-size: 0.875rem;
   }
 
-  .show-more > .link {
+  .show-more {
     background-color: rgba(250, 216, 73, 0.9);
-    opacity: 0;
     transition: opacity 0.25s ease-in-out;
     position: absolute;
+    opacity: 0;
     top: 0;
     left: 0;
     bottom: 0;
     right: 0;
     color: #fff;
     font-size: 18px;
-    font-family: sans-serif;
     text-transform: uppercase;
     text-shadow: 1px 1px 1px #000;
     text-decoration: none;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
-  .show-more > .link:hover {
+  .show-more:hover {
     opacity: 1;
   }
 </style>
